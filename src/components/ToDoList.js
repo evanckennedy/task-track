665ce6719f6df91ee0db8d3ecc8626d7e277c7d9
Evan Kennedy <evanckennedy@protonmail.com>
2024-05-08 20:03:03 -0500

    and functionality to move tasks up and down

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,8 +1,8 @@
-import { FaPen, FaTrash } from 'react-icons/fa';
+import { FaPen, FaTrash, FaArrowUp, FaArrowDown } from 'react-icons/fa';
 import { useState } from "react";
 
 function ToDoList() {
-  const [tasks, setTasks] = useState(['get eggs', 'go shopping', 'go to mall']);
+  const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState('');
 
   function handleInputChange(event) {
@@ -10,13 +10,29 @@ function ToDoList() {
   }
 
   function addTask() {
-    setTasks(t => [...t, newTask]);
+    setTasks(t => [newTask, ...t]);
     setNewTask('');
   }
 
   function deleteTask(index) {
     const updatedTasks = tasks.filter((_, i) => i !== index);
     setTasks(updatedTasks);
+  }
+
+  function moveTaskUp(index) {
+    if (index > 0) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index - 1]] = [updatedTasks[index - 1], updatedTasks[index]];
+      setTasks(updatedTasks);;
+    }
+  }
+
+  function moveTaskDown(index) {
+    if (index < tasks.length - 1) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index + 1]] = [updatedTasks[index + 1], updatedTasks[index]];
+      setTasks(updatedTasks);;
+    }
   }
 
   return (
@@ -46,8 +62,11 @@ function ToDoList() {
           <div key={index} className='task-container flex'>
             <p className='task'>{task}</p>
             <div className="task-controls flex justify-between">
-              <span>May 7, 09:01 a.m</span>
-              <div className="edit-delete flex gap-15">
+              <div className='up-down flex gap-5'>
+                <FaArrowDown className='down-icon' onClick={() => moveTaskDown(index)}/>
+                <FaArrowUp className='up-icon' onClick={() => moveTaskUp(index)}/>
+              </div>
+              <div className="edit-delete flex gap-10">
                 <FaPen className='edit-icon' />
                 <FaTrash className='delete-icon' onClick={() => deleteTask(index)}/>
               </div>
@@ -66,13 +85,8 @@ function ToDoList() {
           </div>   
         </div> */}
 
-
-
       </section>
-      
     </>
-      
-    
   )
 }
 
