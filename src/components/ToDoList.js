1e73d3ed6e8c461312290c6c59af8e045179355d
Evan Kennedy <evanckennedy@protonmail.com>
2024-05-09 20:28:29 -0500

    make each task have it's own timestamp

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -8,9 +8,6 @@ function ToDoList() {
   const [editingText, setEditingText] = useState('');
   const inputRef = useRef(null);
 
-  const date = new Date();
-  const formattedDate = date.toLocaleString('en-US', { month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', hour12: true });
-
   function handleInputChange(event) {
     if (editingTask !== null) {
       setEditingText(event.target.value)
@@ -21,14 +18,16 @@ function ToDoList() {
 
   function addOrSaveTask(event) {
     event.preventDefault();
+    const date = new Date();
+    const formattedDate = date.toLocaleString('en-US', { month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', hour12: true });
     if (editingTask !== null) {
       const updatedTasks = [...tasks];
-      updatedTasks[editingTask] = editingText;
+      updatedTasks[editingTask] = { text: editingText, date: formattedDate };
       setTasks(updatedTasks);
       setEditingTask(null);
       setEditingText('');
     } else if (newTask !== '') {
-      setTasks(t => [newTask, ...t]);
+      setTasks(t => [{ text: newTask, date: formattedDate }, ...t]);
       setNewTask('');
     }
   }
@@ -95,17 +94,17 @@ function ToDoList() {
       <section className="task-grid-container gap-15">
         {tasks.map((task, index) => 
           <div key={index} className='task-container flex'>
-            <p className='task'>{task}</p>
+            <p className='task'>{task.text}</p>
             <div className="task-controls flex justify-between">
               <div className='up-down flex gap-5'>
                 <FaArrowDown className='down-icon' onClick={() => moveTaskDown(index)}/>
                 <FaArrowUp className='up-icon' onClick={() => moveTaskUp(index)}/>
               </div>
               <div>
-                <span>{formattedDate}</span>
+                <span>{task.date}</span>
               </div>
               <div className="edit-delete flex gap-10">
-                <FaPen className='edit-icon' onClick={() => {setEditingTask(index); setEditingText(task);}}/>
+                <FaPen className='edit-icon' onClick={() => {setEditingTask(index); setEditingText(task.text);}}/>
                 <FaTrash className='delete-icon' onClick={() => deleteTask(index)}/>
               </div>
             </div>   
